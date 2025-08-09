33dda6f4820e670a31dd91471c08508a07d58f25
112fm <7artmedia69@gmail.com>
2025-08-09 13:31:27 +0300

    fix: relative paths for GitHub Pages

--- sw.js
+++ sw.js
@@ -1,29 +1,28 @@
-const CACHE_NAME = 'pomodoro-v1';
+const CACHE_NAME = 'pomodoro-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.webmanifest',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => (k!==CACHE_NAME)&&caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME) && caches.delete(k)))
+    )
   );
 });
 
 self.addEventListener('fetch', (e) => {
-  const req = e.request;
   e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req))
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });

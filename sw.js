5bc48e9d20fe3bd1dc400d91c6ae6b7cb277c495
112fm <7artmedia69@gmail.com>
2025-08-09 15:39:12 +0300

    chore: bump SW cache to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pomodoro-v3';
+const CACHE_NAME = 'pomodoro-v4'; // новая версия кэша
 const ASSETS = [
   './',
   './index.html',
@@ -16,7 +16,9 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE_NAME) && caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME) && caches.delete(k)))
+    )
   );
 });
 
